55faeb2b69de0722a99dcfd56ee7f4dab486e6bb
Chris Yeo <chris.yeosj@gmail.com>
2023-09-01 00:01:03 +0800

    Initial Review - 23rd Aug2023

--- README.md
+++ README.md
@@ -1,30 +1,69 @@
-# SkyCastle - ERC 1155 Smart Contract
+# SkyCastle Concepts - ERC 1155 Smart Contract
 
 ## Features
+- ERC1155 NFT
+- Only allow purchases when it is opened for public purchases
+    - `onlyDuringOpenedForPublicPurchase` modifier
 - Only allow purchases, mint and burn if the contract is not paused
 	- `whenNotPaused` modifier
 - Uses [operator-filter-registry](https://github.com/ProjectOpenSea/operator-filter-registry) from OpenSea to enforce royalties
     - Not a foolproof approach
 - Only allow trading the tokens after the release timestamp (trading lockup period)
 	- `onlyAfterRelease` modifier
+    - hardcoded timestamp limit to a maximum of 31st December 2024
 - Allow holders to burn their tokens
     - Once a token is burnt, it should not be part of the collection anymore
+    - tracker to keep track of burnt tokens
+
+## Events Emitted
+- `UriUpdated(string newURI)`
+    - Emitted when Base URI is updated.
+- `ReleaseTimestamp(uint256 newReleaseTimestamp)`
+    - Emitted when Release Timestamp has been changed/updated
+- `OpenForPublicPurchase(bool isPublicPurchaseOpened)`
+    - Emitted when Public Purchase has been changed/Updated
+- `TransferSingle(address indexed operator, address indexed from, address indexed to, uint256 id, uint256 value)`
+    - Emitted when a transfer / mint takes place.
 
 ## Functions
-
-### Public
-- `pause`
-	- `onlyOwner`
-- `unpause`
-	- `onlyOwner`
-- `purchase`
-	- Early access and public sale
+### External
+- `burn`
 	- `whenNotPaused`
+    - Takes a Token Id + Amount. Burns those token and amount if the holder has the correct amount.
+- `setURI`
+	- `onlyOwner`
+    - Updates the base URI, this is important as we'll be using SCAI file system first, and migrating to IPFS when finalized.
+    - contractURI and URI (of token) shares this same base URI
+- `setReleaseTimestamp`
+	- `onlyOwner`
+    - Updates the release timestamp, Only allow trading the tokens after the release timestamp (trading lockup period)
+- `setIsPublicPurchaseOpen`
+    - `onlyOwner`
+    - Updates the Public Purchase Open, so that the Public can start using the purchase function to start minting from the contract
+- `withdraw`
+	- `onlyOwner`
+    - `nonReentrant`
+    - Withdraws the balance ethereum to the owner's address.
+- `purchase`
+	- `whenNotPaused`
+    - `onlyDuringOpenedForPublicPurchase`
+    - Allows a member of the public to purchase/mint an NFT during a public sale
 - `mint`
 	- `onlyOwner`
 	- `whenNotPaused`
+    - Allows the contract owner to "airdrop" tokens to addresses he/she chooses
+- `getTotalBurnedTokensByType`
+    - Tracker to get burnt tokens by token type
+### Public
+- `pause`
+	- `onlyOwner`
+    - Pause is for Emergency Usage only.
+- `unpause`
+	- `onlyOwner`
+    - Unpause is for Emergency Usage following a Pause Scenario.
 - `setRoyaltyInfo`
 	- `onlyOwner`
+    - Updates the Royalty Address + Basis Points in case there are changes.
 - `setApprovalForAll`
 	- Make sure the operator is allowed by `OperatorFilterRegistry`
 	- `onlyAllowedOperatorApproval`
@@ -35,22 +74,45 @@
 	- Make sure the operator is allowed by `OperatorFilterRegistry`
 	- `onlyAfterRelease`
 - `uri`
+    - Takes a _tokenId, returns a URI of the specific token's metadata.
 - `contractURI`
+    - returns a URI of the contract's metadata.
+- `totalMinted`
+    - Total Minted NFTs. Mint only, does not count burnt tokens
 - `totalSupply`
-- `getTotalBurnedTokens`
-### External
-- `burn`
-	- `whenNotPaused`
-- `setURI`
-	- `onlyOwner`
-- `setReleaseTimestamp`
-	- `onlyOwner`
-- `withdraw`
-	- `onlyOwner`
+    - Total Supply for the Current NFTs. Includes minted and burnt tokens.
+
 ### Private
 - `_safeMint`
 	- `whenNotPaused`
+    - a wrapper on the _mint of the ERC1155 Contract and increments SCAI counters / trackers
 - `getCurrentTokenCounter`
+    - Mint Tracker - get current count for specified token id
 - `incrementCurrentTokenCounter`
+    - Mint Tracker - increment current count for specified token id
 
+## Deployment
 
+### Deployment to Development or Production Environments using SCAI API
+1. Ensure your .env is set correctly. `production` points to Ethereum Mainnet, `development` points to Goerli Mainnet. For Test, please set NODE_ENV to `development`
+2. ```npx hardhat compile```
+3. Initialise a Wallet to hold the contract by calling ```createPrivateWallet``` API
+4. Fund the wallet with enough ether. Ensure you're clear what type of ether is needed. (Ether or Goerli Ether)
+5. Deploy Contract on Ethereum Net with the ```deployNFTContract``` API
+
+### Alternative Deployment Method to Development using Remix
+1. Create a Blank Workspace on Remix
+2. Copy and Paste SCAIToken.sol into the contracts folder. (create a contracts folder if it isn't there)
+3. Navigate to Solidity Compiler and choose 0.8.16. Click Compile SCAIToken.sol
+4. Click Deploy and Run Transactions to start the deployment process
+5. Consider testing in the VM Environments Remix has provided
+6. Deploy to Goerli Environments to test Opensea Testnet etc
+7. To Deploy with Metamask, Click Injected Provider - Metamask
+8. Fill in the 3 variables, sample variables could be: 1000, https://public-accessibles.com/dev/metadata/concepts/,  1704067199
+9. Click Transact, approve transaction on metamask and wait for etherscan link to appear
+10. Contract should be deployed.
+
+## Transfer to MultiSig Ownership
+1. Ensure the contract is in correct state. Ensure the Opensea is set correctly before initiating transfer.
+2. Call the TransferOwnership function to the multisig contract. Openzeppelin's Defender will be a good tool to handle this.
+3. https://defender.openzeppelin.com/
--- SCAIToken.sol
+++ SCAIToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-// SCAI NFTs are governed by te following terms and conditions: https://www.skycastle.ai/terms-of-use
+// SCAI NFTs are governed by the following terms and conditions: https://www.skycastle.ai/terms-of-use
 
-pragma solidity ^0.8.9;
+pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
@@ -12,82 +12,110 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "operator-filter-registry/src/DefaultOperatorFilterer.sol";
 
+/**
+ * @author SkycastleAI Team
+ * @title Skycastle Concepts NFT
+ * @dev By default, the owner account will be the one that deploys the contract. This will be
+ * changed with {transferOwnership} to a multisig to secure the NFT Contract. The transfer will occur
+ * AFTER setting up the basics like Opensea Collections etc.
+ */
 contract SCAIToken is ERC1155, ERC2981, Ownable, Pausable, ReentrancyGuard, DefaultOperatorFilterer {
     using Counters for Counters.Counter;
 
-    uint256 constant MAX_SUPPLY = 480;
-
-    uint256 public constant maxPerTx = 1;
-    uint256 public constant mintPrice = 0.001 ether;
-
+    string public constant name = "SCAI Concepts";
+    uint256 public constant MINT_PRICE = 0.001 ether;
+    uint256 private constant MAX_SUPPLY = 480;
     uint256 private constant MAX_MINT_PER_CHARACTER = 60;
     uint256 private constant AMOUNT_OF_DIFFERENT_CHARACTERS = 8;
+    /// GMT Time - end of 2024
+    uint256 private constant TIMESTAMP_END_OF_2024 = 1735689599;
 
-    string public name = "SCAI Concepts"; // please change this before prod
-
-    // Base URI
+    /// Base URI
     string private baseURI;
 
-    // unix timestamp of trading date
+    /// unix timestamp of trading date
     uint256 public releaseTimestamp;
 
-    // The mapping from token type id to the number of tokens of that type
+    /// flag for when public purchase allowed
+    bool public isPublicPurchaseOpened;
+
+    /**
+    * @notice counter for the mapping of token type id to the number of tokens of that type
+    * @dev this does not change even when theres burning occuring
+    */
     mapping(uint256 => Counters.Counter ) private _tokenCounts;
 
-    // tracker for total burned tokens
-    mapping(uint256 => uint256) private totalBurnedTokens;
+    /// tracker for total & burned tokens
+    mapping(uint256 => uint256) private _totalSupply;
 
+    /// tracker for total burned tokens
+    mapping(uint256 => uint256) private _totalBurnedTokens;
+
+    /// emit events for critical state updates
+    /// For URI updating
+    event UriUpdated(string newURI);
+    /// For Release Timestamp Updates
+    event ReleaseTimestamp(uint256 newReleaseTimestamp);
+    /// For Opened to Public Purchase Updates
+    event OpenForPublicPurchase(bool isPublicPurchaseOpened);
+
+    /// Modifier to Prevent Trading / Transfer of NFT until after releaseTimestamp
     modifier onlyAfterRelease() {
         require(block.timestamp >= releaseTimestamp, "Trading is not allowed until release");
         _;
     }
 
+    /// Modifier to Prevent Public purchase of NFT until after isPublicPurchaseOpened is set to true
+    modifier onlyDuringOpenedForPublicPurchase() {
+        require(isPublicPurchaseOpened, "Please wait for public purchase to open");
+        _;
+    }
+
+    /**
+    * @dev constructor
+    *
+    * @param _royaltyFeesInBasisPoints // value of 100 to 1000 (in bips) 1000 = 10%
+    * @param _contractURI // base url of the metadata. this will be in IPFS
+    * @param _releaseTimestamp //unix timestamp, must be before end december 2024
+    */
     constructor(uint16 _royaltyFeesInBasisPoints, string memory _contractURI, uint256 _releaseTimestamp) ERC1155(_contractURI) {
+        // validating release timestamp here, and not using setReleaseTimestamp because it should not emit an event in constructor
+        require(_releaseTimestamp <= TIMESTAMP_END_OF_2024, "Please use a timestamp BEFORE end of 2024.");
+
         baseURI = _contractURI;
         setRoyaltyInfo(msg.sender, _royaltyFeesInBasisPoints);
         releaseTimestamp = _releaseTimestamp;
-    }
-
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
+        isPublicPurchaseOpened = false;
     }
 
     /**
-    * @notice global purchase function used in early access and public sale
+    * @notice Purchase function is used in public sale when it gets opened by scai
+    * @dev this is dependent on isPublicPurchaseOpened variable for public access
     *
-    * @param amount the amount of tokens to purchase
     * @param tokenId the id of token type to mint
     */
-    function purchase(uint256 amount, uint256 tokenId) payable public whenNotPaused {
-
+    function purchase(uint256 tokenId) payable external whenNotPaused onlyDuringOpenedForPublicPurchase {
         // Check if the user has the required funds
-        require(amount > 0 && amount <= maxPerTx, "Purchase: amount prohibited");
-        require(msg.value == amount * mintPrice, "Purchase: Incorrect payment");
-        require(totalSupply() + amount <= MAX_SUPPLY, "Mint: Max supply reached");
+        require(msg.value == MINT_PRICE, "Purchase: Incorrect payment");
+        require(totalMinted() + 1 <= MAX_SUPPLY, "Mint: Max supply reached");
         // get current token count
         uint256 currentTokenCount = getCurrentTokenCounter(tokenId);
         // test for overflow.
-        require((currentTokenCount + amount) <= MAX_MINT_PER_CHARACTER, "Mint: Max supply reached for character type");
-        require(tokenId >= 0 && tokenId < AMOUNT_OF_DIFFERENT_CHARACTERS, "Error: Token Type Incorrect");
-
-        _safeMint(msg.sender, tokenId, amount);
+        require((currentTokenCount + 1) <= MAX_MINT_PER_CHARACTER, "Mint: Max supply reached for character type");
+        _safeMint(msg.sender, tokenId, 1);
     }
 
     /**
-    * @dev Wrapping the mint function for airdrop
+    * @notice this is for the contract owner to airdrop tokens to specific users.
+    * @dev wrapping the mint function for airdrop
     *
     * @param toAddress //address of eth to send to
     * @param tokenId // the token type to mint
     * @param amount  //amount of tokens to mint for account
     */
-    function mint(address toAddress, uint256 tokenId, uint256 amount) public onlyOwner whenNotPaused
+    function mint(address toAddress, uint256 tokenId, uint256 amount) external onlyOwner whenNotPaused
     {
-        require(tokenId >= 0 && tokenId < AMOUNT_OF_DIFFERENT_CHARACTERS, "Error: Token Type Incorrect");
-        require(totalSupply() + amount <= MAX_SUPPLY, "Mint: Max supply reached");
+        require(totalMinted() + amount <= MAX_SUPPLY, "Mint: Max supply reached");
         uint256 currentTokenCount = getCurrentTokenCounter(tokenId);
         require((currentTokenCount + amount ) <= MAX_MINT_PER_CHARACTER, "Mint: Max supply reached for character type");
 
@@ -95,44 +123,89 @@ contract SCAIToken is ERC1155, ERC2981, Ownable, Pausable, ReentrancyGuard, Defa
     }
 
     /**
+     * @notice Burn Function to reduce total supply.
      * @dev burn function for sender.
-     * @param _tokenId // the token type to mint
-     * @param _amount  //amount of tokens to mint for account
+     * @param _tokenId // the token type to burn
+     * @param _amount  //amount of tokens to burn for account
      */
     function burn(uint256 _tokenId, uint256 _amount) external whenNotPaused {
         require(balanceOf(msg.sender, _tokenId) >= _amount, "Insufficient balance");
         _burn(msg.sender, _tokenId, _amount);
-        totalBurnedTokens[_tokenId] += _amount;
+        _totalBurnedTokens[_tokenId] += _amount;
+        _totalSupply[_tokenId] -= _amount;
     }
 
-
     /**
-     * @dev calls _mint of the ERC1155 contract.
+     * @dev Updates the base URI that will be used to retrieve metadata.
+     * @param _newuri The base URI to be used.
      */
-    function _safeMint(address to, uint256 tokenId, uint256 amount) private whenNotPaused
+    function setURI(string memory _newuri) external onlyOwner
     {
-        _mint(to, tokenId, amount, "");
-        //increment appropriate counters
-        incrementCurrentTokenCounter(tokenId, amount);
+        baseURI = _newuri;
+        emit UriUpdated(_newuri);
     }
 
     /**
-     * @dev Allows the owner to update the royalty information
+     * @notice SCAI has regulatory compliance on when this NFT can be traded. New Values cannot be beyond 31st Dec 2024
+     * @dev Updates the releaseTimestamp should situation change.
+     * @param _newTimestamp The new timestamp that should be used.
      */
-    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        internal
-        whenNotPaused
-        override(ERC1155)
-    {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    function setReleaseTimestamp(uint256 _newTimestamp) external onlyOwner {
+        require(_newTimestamp <= TIMESTAMP_END_OF_2024, "Please use a timestamp BEFORE end of 2024.");
+        releaseTimestamp = _newTimestamp;
+        emit ReleaseTimestamp(_newTimestamp);
     }
 
     /**
+     * @notice Allows the owner to open or close public purchases for NFT of this contract
+     * @dev Updates the condition on whether public purchases is opened.
+     * @param _isPublicPurchaseOpened boolean flag to toggle open or close
+     */
+    function setIsPublicPurchaseOpen(bool _isPublicPurchaseOpened) external onlyOwner
+    {
+        isPublicPurchaseOpened = _isPublicPurchaseOpened;
+        emit OpenForPublicPurchase(_isPublicPurchaseOpened);
+    }
+
+    /**
+     * @notice tracker to obtain total burned tokens by token type
+     * @param _tokenId the token type
+     */
+    function getTotalBurnedTokensByType(uint256 _tokenId) external view returns (uint256) {
+        return _totalBurnedTokens[_tokenId];
+    }
+
+    /**
+     * @dev Withdraws the amount to owner address
+     */
+    function withdraw() external onlyOwner nonReentrant
+    {
+        uint256 balance = address(this).balance;
+        Address.sendValue(payable(owner()), balance);
+    }
+
+    /**
+     * @notice for the pausing of contract in emergencies only
+     */
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @notice for the unpausing of contract when emergencies are rectified
+     */
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    /**
+     * @notice Updates the Royalty Amount should situation changes
      * @dev Allows the owner to update the royalty information
+     * @param _receiver the receiver of the royalty
+     * @param _royaltyFeesInBasisPoints in basis points. ie. 1000 bips = 10%
      */
     function setRoyaltyInfo(address _receiver, uint16 _royaltyFeesInBasisPoints) public onlyOwner
     {
-        require(_royaltyFeesInBasisPoints > 0 && _royaltyFeesInBasisPoints < 10000, "Invalid Royalty Fees in Bips");
         _setDefaultRoyalty(_receiver, _royaltyFeesInBasisPoints);
     }
 
@@ -183,32 +256,7 @@ contract SCAIToken is ERC1155, ERC2981, Ownable, Pausable, ReentrancyGuard, Defa
     }
 
     /**
-     * @dev Updates the base URI that will be used to retrieve metadata.
-     * @param _newuri The base URI to be used.
-     */
-    function setURI(string memory _newuri) external onlyOwner
-    {
-        baseURI = _newuri;
-    }
-
-    /**
-     * @dev Updates the releaseTimestamp should situation change.
-     * @param _newTimestamp The new timestamp that should be used.
-     */
-    function setReleaseTimestamp(uint256 _newTimestamp) external onlyOwner {
-        releaseTimestamp = _newTimestamp;
-    }
-
-    /**
-     * @dev Withdraws the amount to owner
-     */
-    function withdraw() external onlyOwner nonReentrant 
-    {
-        uint256 balance = address(this).balance;
-        Address.sendValue(payable(owner()), balance);
-    }
-
-    /**
+     * @notice gets the metadata uri of the specific token id. eg. https://public-accessibles.com/dev/metadata/concepts/1.json
      * @dev concatenates the baseUri with the tokenId to get the token specific uri
      * @param _tokenId The token id (nft type) of token
      */
@@ -225,7 +273,8 @@ contract SCAIToken is ERC1155, ERC2981, Ownable, Pausable, ReentrancyGuard, Defa
     }
 
     /**
-     * @dev gets the contract uri of the project.
+     * @notice gets the contract uri of the project. eg. https://public-accessibles.com/dev/metadata/concepts/contractMetadata.json
+     * @dev concatenates the base uri and contractMetadata.json to derive the contract Uri
      */
     function contractURI() public view returns (string memory) {
         return string (
@@ -237,16 +286,64 @@ contract SCAIToken is ERC1155, ERC2981, Ownable, Pausable, ReentrancyGuard, Defa
     }
 
     /**
-     * @dev gets current token counter of the supply
-     * @param _tokenId The token id (nft type) of token
+     * @notice gets the total minted NFTs of the NFT Project
+     * @dev uses the _tokenCounts, which includes mint only
      */
-    function getCurrentTokenCounter(uint256 _tokenId) private view returns(uint256)
+    function totalMinted() public view virtual returns (uint256)
     {
-        require(_tokenId >= 0 && _tokenId < AMOUNT_OF_DIFFERENT_CHARACTERS, "Incorrect Token Type");
-        return _tokenCounts[_tokenId].current();
+        uint256 currentSupply = 0;
+        // Initialize the token counts
+        for (uint256 i = 0; i < AMOUNT_OF_DIFFERENT_CHARACTERS; i++) {
+            currentSupply += (_tokenCounts[i].current());
+        }
+        return currentSupply;
     }
 
     /**
+     * @notice check the total supply
+     * @dev uses the _totalSupply that includes mint + burn
+     */
+    function totalSupply() public view virtual returns (uint256)
+    {
+        uint256 totalTokens = 0;
+        // Initialize the token counts
+        for (uint256 i = 0; i < AMOUNT_OF_DIFFERENT_CHARACTERS; i++) {
+            totalTokens += _totalSupply[i];
+        }
+        return totalTokens;
+    }
+
+    /**
+     * @dev Hook function that is called before any token transfer.
+     *
+     * This function is part of the ERC-1155 standard and is used to implement additional checks or
+     * perform actions before a token transfer occurs.
+     */
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        whenNotPaused
+        override(ERC1155)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    /**
+     * @notice Calls the _mint of the ERC1155 Contract and Increment SCAI Counters on Mint.
+     * @dev calls _mint of the ERC1155 contract.
+     * @param _to // receiver
+     * @param _tokenId // token type
+     * @param _amount // amount of tokens to mint
+     */
+    function _safeMint(address _to, uint256 _tokenId, uint256 _amount) private whenNotPaused
+    {
+        _mint(_to, _tokenId, _amount, "");
+        //increment appropriate counters
+        incrementCurrentTokenCounter(_tokenId, _amount);
+        _totalSupply[_tokenId] += _amount;
+    }
+
+    /**
+     * @notice increment the tracker of Token Supply for the specified token Id
      * @dev increment the token counter of the supply of a specific tokenId
      * @param _tokenId The token id (nft type) of token
      * @param _amount The amount to increment
@@ -260,23 +357,13 @@ contract SCAIToken is ERC1155, ERC2981, Ownable, Pausable, ReentrancyGuard, Defa
     }
 
     /**
-     * @dev gets the total supply of the NFT Project
+     * @notice Get current token supply of specified token id.
+     * @dev gets current token counter of the supply
+     * @param _tokenId The token id (nft type) of token
      */
-    function totalSupply() public view virtual returns (uint256) 
+    function getCurrentTokenCounter(uint256 _tokenId) private view returns(uint256)
     {
-        uint256 currentSupply = 0;
-        // Initialize the token counts
-        for (uint256 i = 0; i < AMOUNT_OF_DIFFERENT_CHARACTERS; i++) {
-            currentSupply += (_tokenCounts[i].current());
-        }
-        return currentSupply;
-    }
-
-    /**
-     * @dev get total burned tokens by everyone
-     * @param _tokenId the token type
-     */
-    function getTotalBurnedTokens(uint256 _tokenId) public view returns (uint256) {
-        return totalBurnedTokens[_tokenId];
+        require(_tokenId >= 0 && _tokenId < AMOUNT_OF_DIFFERENT_CHARACTERS, "Incorrect Token Type");
+        return _tokenCounts[_tokenId].current();
     }
 }
